e286678aaa4f51b264ca01af96e075ffa5042700
yfyang86 <yfyang.86@gmail.com>
2020-02-05 00:53:06 +0800

    Plots

--- R/nCov2019.R
+++ R/nCov2019.R
@@ -88,3 +88,112 @@ extract_province <- function(object, i, by) {
   return(y$data)  
 }
 
+#' This is data to be included in the nCov2019 package
+#'
+#' @name chinamaps
+#' @docType data
+#' @author Yifan Yang \email{yfyang.86@gmail.com}
+#' @references \url{www.stats.gov.cn}
+#' @keywords data
+NULL
+
+
+
+#' @importFrom magrittr %>%
+#' @importFrom data.table data.table
+#' @importFrom data.table merge
+#' @import ggplot2
+#' @import wesanderson
+#' @import mapdata
+#' @method plot nCov2019
+#' @return nCov2019 list
+#' @export
+#' @param x nCov2019 Object
+#' @param chinamaps Data Frame
+#' @examples
+#' # y = plot(get_nCov2019(), chinamaps)
+#' # print(y$val)
+plot.nCov2019 <-function(x, chinamaps){
+  udf_1<-Vectorize(
+    function(x,val=2) {
+      x %>% strsplit(split = '') %>% unlist %>% length() > val}
+    ,vectorize.args = 'x'
+  )
+  
+  
+  chinamaps.db <- data.table(chinamaps)
+  #cat("PHASE 1") 
+  chinamaps.db[LevelType==1,]$"ShortName" %>% as.character() -> prov_list
+  
+  city_db <- data.table(names="", lat=0, long=0, confirmed=0)
+  
+  u <- NULL
+  
+  for (i in prov_list){
+    tryCatch( (u = rbind(u,cbind(i[1],x[i,]))), error=function(cond) {warning('no data')})
+  }
+  
+  #cat("PHASE 2")
+  u <- data.table(u)
+  names(u)[1] = "ProvinceName"
+  names(u)[2] = "ShortName"
+  u$ShortName<- as.character(u$ShortName)
+  u$ShortName<-sub('*区$','',u$ShortName)
+  u$ShortName[u$ShortName %>% udf_1() %>% as.vector()] <- sub('*县$','',u[u$ShortName %>% udf_1() %>% as.vector(),ShortName])
+  u$ShortName<-sub('*自治州$','',u$ShortName)
+  u$ShortName[u$ShortName %>% udf_1() %>% as.vector()] <-  sub('*州$','',u[u$ShortName %>% udf_1() %>% as.vector(),ShortName])
+  u$ShortName[u$ShortName %>% udf_1() %>% as.vector()] <-  sub('*市$','',u[u$ShortName %>% udf_1() %>% as.vector(),ShortName])
+  
+  z <- merge(u, chinamaps.db, by = "ShortName", all = TRUE)[confirm>0,]
+  
+  mat.cities = z[!is.na(lng),c("ShortName", "Lat", "lng", "confirm", "ProvinceName","ID")]
+  
+  mat.cities_db = mat.cities[mat.cities[,max(as.integer(ID)), by=ShortName], nomatch=0L, on = c(ID="V1")][,1:5]
+  
+  china <- map("china", plot = F)
+  
+  a<-list(date=format(Sys.time(), "%Y%b%d"), val=NULL, val2=NULL)
+  
+  a$val2 = ggplot() + 
+    geom_path(data = china, aes(long, lat, group = group), color = '#3161A4', show.legend = F) +
+    geom_point(data = mat.cities_db
+               , aes(x = lng, y = Lat
+                     , size = confirm
+                     , color=ceiling(log(mat.cities_db$confirm))+1
+               ), alpha = 0.8) + 
+    scale_color_continuous( limits=c(1, max(ceiling(log(mat.cities_db$confirm))+1))
+                            , breaks=seq(1, max(ceiling(log(mat.cities_db$confirm))+1), by=1)
+                            , low = 'blue', high = 'red'
+    ) +
+    guides(color= guide_legend(), size=guide_legend()) + 
+    theme_bw() + 
+    theme(legend.position = "none", 
+          axis.line = element_blank(),
+          #panel.grid.major = element_blank(),
+          #panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank()) +  
+    ggtitle(paste("Date",format(Sys.time())))
+  
+  a$val = ggplot() + 
+    geom_path(data = china, aes(long, lat, group = group), color = '#3161A4', show.legend = F) +
+    geom_point(data = mat.cities_db
+               , aes(x = lng, y = Lat
+                     , size = confirm
+                     , color= ProvinceName
+               ), alpha = 0.8) + 
+    guides(color= guide_legend(), size=guide_legend()) + 
+    scale_fill_manual(values = wes_palette("Zissou1", 36, type = "continuous")) +
+    theme_bw() + 
+    theme(legend.position = "none", 
+          axis.line = element_blank(),
+          #panel.grid.major = element_blank(),
+          #panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank()) +  
+    ggtitle(paste("Date",format(Sys.time())))
+  return(a);
+}
+
+
+
--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 具体使用方法，请参考<https://mp.weixin.qq.com/s/_0D8ENb-4lGm4UV16Ok28A>.
 
+## GIS
+
+```R
+library(mapdata)
+library(nCov2019)
+data(chinamaps)
+
+data_current = get_nCov2019()
+y = plot(data_current, chinamaps)
+
+# Figure 1: By Province - No import 
+print(y$val)
+# Figure 2: By Size - No import
+print(y$val)
+```
+
+![fig1](./man/v1.png)
+![fig2](./man/v2.png)
+
